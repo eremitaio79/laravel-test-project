13b9314c10b2410d6002091a794a1ef42708b3f3
Paulo Eremita <eremitaio79@gmail.com>
2024-11-08 13:04:38 -0300

    base project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Projeto 1 - Laravel 10x</title>
+
+    </head>
+
+
+    <body>
+    
+        <h3>Projeto Laravel 10x</h3>
+
+    </body>
+</html>
