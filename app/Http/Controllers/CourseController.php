270ec8939b069631ae5abacac26c3f3d40088663
Paulo Eremita <eremitaio79@gmail.com>
2024-11-11 11:01:27 -0300

    rotas, controllers e views de cursos

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -6,9 +6,73 @@ use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    // List all courses.
+
+    /**
+     * e79: Página iniciar de cursos. Apresenta a lista dos cursos cadastrados.
+     */
     public function index()
     {
+        // dd('index course controller.');
 
+        return view('courses.index');
+    }
+
+
+    /**
+     * e79: Detalhes do curso selecionado.
+     */
+    public function show()
+    {
+        // dd('Show');
+        return view('courses.show');
+    }
+
+
+    /**
+     * e79: Carrega o formulário de cadastro de cursos.
+     */
+    public function create()
+    {
+        // dd('Create');
+        return view('courses.create');
+    }
+
+
+    /**
+     * e79: Faz o insert do novo curso no banco de dados.
+     */
+    public function store()
+    {
+        // dd('Store');
+        // e79: Este método não possui view. Ele somente vai redirecionar após o insert.
+    }
+
+
+    /**
+     * e79: Carrega o formulário de edição de cursos.
+     */
+    public function edit()
+    {
+        // dd('Edit');
+        return view('courses.edit');
+    }
+
+
+    /**
+     * 
+     */
+    public function update()
+    {
+        // dd('Update');
+        // e79: Este método não possui view. Ele somente vai redirecionar após o update.
+    }
+
+
+    /**
+     * e79: Faz a exclusão do curso selecionado.
+     */
+    public function destroy()
+    {
+        // dd('Delete');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// My routes.
-// Courses.
-Route::get('/index-course', [CourseController::class, 'index']);
+// e79: My routes.
+// e79: Courses.
+Route::get('/index-course', [CourseController::class, 'index']); // e79: Index de cursos. Lista todos os cursos.
+Route::get('/show-course', [CourseController::class, 'show']); // e79: Página que apresenta os detalhes do curso selecionado.
+Route::get('/create-course', [CourseController::class, 'create']); // e79: Página de form para inserção de novos cursos.
+Route::post('/store-course', [CourseController::class, 'store']); // e79: Insert do novo curso.
+Route::get('/edit-course', [CourseController::class, 'edit']); // e79: Página de form para editar os dados do curso.
+Route::put('update-course', [CourseController::class, 'update']); // e79: Update do curso selecionado.
+Route::delete('destroy-course', [CourseController::class, 'destroy']); // e79: Deleta o curso selecionado.
