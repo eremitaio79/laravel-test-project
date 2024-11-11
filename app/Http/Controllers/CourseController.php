7b712d2c042174bf20c676ee46348fa1c7f383aa
Paulo Eremita <eremitaio79@gmail.com>
2024-11-11 09:08:15 -0300

    controller e rota para courses

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    // List all courses.
+    public function index()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// My routes.
+// Courses.
+Route::get('/index-course', [CourseController::class, 'index']);
